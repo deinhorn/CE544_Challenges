39805a6eeaca0b7eeb900783979441783a00fbd0
mlkromis <mlkromis@bu.edu>
2016-12-09 01:16:16 -0500

    Update crawlerNode.js

--- Challenge-8/crawlerNode.js
+++ Challenge-8/crawlerNode.js
@@ -1,27 +1,43 @@
 var SerialPort = require("serialport");
 var app = require('express')();
-var xbee_api = require('xbee-api');
-
+//var xbee_api = require('xbee-api');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/*
 var C = xbee_api.constants;
 var XBeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2
 });
-
-var portName = process.argv[2];
-
+*/
+var ArduinoPort = process.argv[2];
+//var XbeePort = process.argv[3];
 var sampleDelay = 3000;
 
-
+/*
 //Note that with the XBeeAPI parser, the serialport's "data" event will not fire when messages are received!
-portConfig = {
+XbeeConfig = {
 	baudRate: 9600,
   parser: XBeeAPI.rawParser()
 };
+*/
+ArduinoConfig = {
+  baudRate: 9600,
+  parser: SerialPort.parsers.readline("\n")
+  //parser: SerialPort.parsers.readline("\n")
+};
+/*
+var XbeeSP;
+XbeeSP = new SerialPort.SerialPort(XbeePort, XbeeConfig);
+*/
 
-var sp;
-sp = new SerialPort.SerialPort(portName, portConfig);
+var ArduinoSP;
+ArduinoSP = new SerialPort.SerialPort(ArduinoPort, ArduinoConfig);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname +'/keyPressTest.html');
+});
 
+/*
 //Create a packet to be sent to all other XBEE units on the PAN.
 // The value of 'data' is meaningless, for now.
 var RSSIRequestPacket = {
@@ -33,17 +49,47 @@ var RSSIRequestPacket = {
 }
 
 var requestRSSI = function(){
-  sp.write(XBeeAPI.buildFrame(RSSIRequestPacket));
+  XbeeSP.write(XBeeAPI.buildFrame(RSSIRequestPacket));
 }
+*/
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
-sp.on("open", function () {
-  console.log('open');
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('control', function(msg){
+    //io.emit('control',msg);
+    ArduinoSP.write(msg);
+    console.log(msg);
+  });
+});
+
+/*
+XbeeSP.on("open", function () {
+  console.log('Xbee Port Open');
   requestRSSI();
   setInterval(requestRSSI, sampleDelay);
 });
-
+*/
+ArduinoSP.on("open", function(){
+  console.log('Arduino Port Open');
+    ArduinoSP.on('data', function(data) {
+    //console.log('data received: ' + data);
+    io.emit('control', data);
+  });
+});
+/*
 XBeeAPI.on("frame_object", function(frame) {
   if (frame.type == 144){
     console.log("Beacon ID: " + frame.data[1] + ", RSSI: " + (frame.data[0]));
   }
+  /*
+  //THIS IS WHERE LOCALIZATION CODE GOES
+
+  //SEND DATA TO HTML SCRIPT
+  var posInfo = [hallwayNum,hallLen];
+  io.emit('posUpdated',posInfo);
 });
+*/
